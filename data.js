0623b17649d914ae64881f61ac445ed8f13e5e14
gn1884728 <124494345+gn1884728@users.noreply.github.com>
2026-06-25 07:04:50 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+const hotels = [
+    { region: 'tokyo', name: '東橫INN - 上野田原町站', id: '688581' },
+    { region: 'tokyo', name: '相鐵 FRESA INN - 東新宿站前', id: '688315' },
+    { region: 'tokyo', name: '東京上野喜來登福朋飯店', id: '1458598' },
+    { region: 'fukuoka', name: 'lyf 天神福岡飯店', id: '72229900' },
+    { region: 'fukuoka', name: '福岡日航飯店', id: '993535' },
+    { region: 'osaka', name: '大阪都喜來登飯店', id: '993284' },
+    { region: 'osaka', name: '東橫 INN - 大阪難波', id: '124671917' },
+    { region: 'sapporo', name: 'Vessel Inn 札幌中島公園飯店', id: '705328' },
+    { region: 'sapporo', name: '洲至奢選洞爺溫莎飯店', id: '711304' }
+];
